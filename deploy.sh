a84b473e1869f0076e31ad0917f766fa7feb634d
Daniel Minear <dan@minear.name>
2015-09-28 07:22:06 -0700

    Added comments

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# copy the Apache handler to somewhere in the Perl library strucure
+# probably need to 'sudo' this script if you are a regular user
 
 cp DanHandler.pm /usr/local/lib/site_perl/ModPerl/DanHandler.pm
 chown root:root  /usr/local/lib/site_perl/ModPerl/DanHandler.pm
 chmod 644  /usr/local/lib/site_perl/ModPerl/DanHandler.pm
-
