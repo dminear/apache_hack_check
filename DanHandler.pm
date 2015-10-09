a48e1ee8a3862d69b4da4f80bd2db1311de147a3
Daniel Minear <dan@minear.name>
2015-10-09 08:17:09 -0700

    Updated to handle Redis errors.

--- DanHandler.pm
+++ DanHandler.pm
@@ -1,70 +1,57 @@
 package ModPerl::DanHandler;
+
 use strict;
 use warnings;
 use FileHandle;
 use IO::Socket::INET;
-
 use Apache2::Log;
 use Apache2::RequestRec ();
 use Apache2::Connection ();
-#use Redis::Client;
 use Redis;
-
 use Apache2::Const -compile => qw(FORBIDDEN OK :log);
 
-my $redis;
+my $redis = Redis->new(	host=>'localhost',
+						port=>6379,
+						password => "123qwe!@#QWE" );
 
-BEGIN {
-#mkdir "/var/www/bad_ips";
-#chmod 0777, "/var/www/bad_ips";
-$redis = Redis->new(host=>'localhost', port=>6379);
-$redis->auth("123qwe!@#QWE");
-}
-
-#my $ipstor = "/var/www/bad_ips";
-
-my $sock = IO::Socket::INET->new(PeerPort => 8125,
-				PeerAddr => '127.0.0.1',
-				Proto => 'udp');
+# statsd socket
+my $sock = IO::Socket::INET->new(	PeerPort => 8125,
+									PeerAddr => '127.0.0.1',
+									Proto => 'udp' );
 
 sub handler {
 	my $r = shift;
 	
-	#my $str = $r->connection->remote_ip();
 	my $str = $r->connection->client_ip();
 	my $rlog = $r->log;
 
+	# let these IPs through anytime
 	if ( $str =~ /71.165/ || 
 		$str =~ /192.168.0/  ||
 		$str =~ /130.76/  ||
-		$str =~ /108.0/ ) {
+		$str =~ /108.0/
+		) {
 		return Apache2::Const::OK;
 	}
 
-	$redis->auth("123qwe!@#QWE");
-	# if there is an attempt to access "zencart/admin", then put the ip on the block list
+	# if there is an attempt to access "zencart/admin" or other attempts, 
+	# then put the ip on the block list
 	if ($r->unparsed_uri() =~ /zencart\/admin$/ ||
 		$r->unparsed_uri() =~ /zencart\/+admin\/+/ ||
 		$r->unparsed_uri() =~ /phpbb2/i  ||
 		$r->unparsed_uri() =~ /wp-login/  ||
 		$r->unparsed_uri() =~ /wordpress\/xmlrpc.php/  ||
 		$r->unparsed_uri() =~ /phpMyAdmin/ ) {
-		#$r->log_error("BAD IP: $str");
 		$sock->send( "hacker.unparsed_uri." . $r->unparsed_uri() . ":1|c\n" ) if defined $sock;
-
-		#my $fh = FileHandle->new( "> $ipstor/$str");
-		#if (defined $fh) {
-		#	print $fh time();
-		#	$fh->close;
-		#}
-
-		$rlog->notice("Bad IP ", $str, " putting in redis");
-		$redis->hincrby( 'badips', $str, 1 );
+		if ($redis) {
+			$rlog->notice("Bad IP ", $str, " putting in redis");
+			$redis->auth("123qwe!@#QWE");
+			$redis->hincrby( 'badips', $str, 1 );
+		}
 	}
 	
 	# check the block list
-	#if (-e "$ipstor/$str") {
-	if ($redis->hexists( 'badips', $str )) {
+	if (defined $redis && $redis->hexists( 'badips', $str )) {
 		$sock->send( "request.blocked:1|c\n" ) if defined $sock;
 		$rlog->notice("Bad IP ", $str, " blocked");
 		return Apache2::Const::FORBIDDEN;
@@ -73,8 +60,8 @@ sub handler {
 		my $name = $r->hostname();
 		$name =~ tr/./-/;
 		$sock->send( "request.hostname." . $name . ":1|c\n" ) if defined $sock;
-		#$rlog->notice("OK IP ", $str, " permitting");
 		return Apache2::Const::OK;
 	}
 }
+
 1;
--- README.md
+++ README.md
@@ -5,8 +5,8 @@ Perl module for Apache mod_perl to check frequent attack vectors
 I noticed some common URL requests coming in for particular URLs. Rather than waste
 time with processing these, I wanted to capture the IPs and then do some
 analysis. It also helps Apache so it doesn't have to keep processing the same
-bad requests and spend compute cycles to get to an error status.
-
+bad requests and spend compute cycles to get to an error status. The request is
+rejected at the beginning of the request cycle.
 
 ## Somewhere in your Apache configuration
 
