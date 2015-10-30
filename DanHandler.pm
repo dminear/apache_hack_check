bd48419176aeb6506d3975427a9273c0ccf5948c
Daniel Minear <dan@minear.name>
2015-10-30 07:22:10 -0700

    Updated logic so a check is done first.

--- DanHandler.pm
+++ DanHandler.pm
@@ -10,14 +10,15 @@ use Apache2::Connection ();
 use Redis;
 use Apache2::Const -compile => qw(FORBIDDEN OK :log);
 
+my $pw = '123qwe!@#QWE';
 my $redis = Redis->new(	host=>'localhost',
-						port=>6379,
-						password => "123qwe!@#QWE" );
+				port=>6379,
+				password => $pw );
 
 # statsd socket
 my $sock = IO::Socket::INET->new(	PeerPort => 8125,
-									PeerAddr => '127.0.0.1',
-									Proto => 'udp' );
+					PeerAddr => '127.0.0.1',
+					Proto => 'udp' );
 
 sub handler {
 	my $r = shift;
@@ -34,34 +35,36 @@ sub handler {
 		return Apache2::Const::OK;
 	}
 
+	# check the block list to get out as soon as possible if there
+	if (defined $redis && $redis->hexists( 'badips', $str )) {
+		$sock->send( "request.blocked:1|c\n" ) if defined $sock;
+		$rlog->notice("Bad IP ", $str, " blocked");
+		return Apache2::Const::FORBIDDEN;
+	}
+
 	# if there is an attempt to access "zencart/admin" or other attempts, 
 	# then put the ip on the block list
 	if ($r->unparsed_uri() =~ /zencart\/admin$/ ||
 		$r->unparsed_uri() =~ /zencart\/+admin\/+/ ||
 		$r->unparsed_uri() =~ /phpbb2/i  ||
 		$r->unparsed_uri() =~ /wp-login/  ||
+		$r->unparsed_uri() =~ /xmlrpc.php/  ||
 		$r->unparsed_uri() =~ /wordpress\/xmlrpc.php/  ||
 		$r->unparsed_uri() =~ /phpMyAdmin/ ) {
 		$sock->send( "hacker.unparsed_uri." . $r->unparsed_uri() . ":1|c\n" ) if defined $sock;
 		if ($redis) {
 			$rlog->notice("Bad IP ", $str, " putting in redis");
-			$redis->auth("123qwe!@#QWE");
+			$redis->auth($pw);
 			$redis->hincrby( 'badips', $str, 1 );
 		}
+		return Apache2::Const::FORBIDDEN;
 	}
 	
-	# check the block list
-	if (defined $redis && $redis->hexists( 'badips', $str )) {
-		$sock->send( "request.blocked:1|c\n" ) if defined $sock;
-		$rlog->notice("Bad IP ", $str, " blocked");
-		return Apache2::Const::FORBIDDEN;
-	} else {
-		$sock->send( "request.allowed:1|c\n" ) if defined $sock;
-		my $name = $r->hostname();
-		$name =~ tr/./-/;
-		$sock->send( "request.hostname." . $name . ":1|c\n" ) if defined $sock;
-		return Apache2::Const::OK;
-	}
+	$sock->send( "request.allowed:1|c\n" ) if defined $sock;
+	my $name = $r->hostname();
+	$name =~ tr/./-/;
+	$sock->send( "request.hostname." . $name . ":1|c\n" ) if defined $sock;
+	return Apache2::Const::OK;
 }
 
 1;
