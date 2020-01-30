441a474ff1fbbd0a55ba67eb97fe43a0610581d7
Dan Minear <dan@minear.name>
2020-01-30 13:55:23 -0800

    Added checking of X-Forwarded-For for Cloudflare and Redis rate limiting.

--- DanHandler.pm
+++ DanHandler.pm
@@ -7,6 +7,7 @@ use IO::Socket::INET;
 use Apache2::Log;
 use Apache2::RequestRec ();
 use Apache2::Connection ();
+use APR::Table ();
 use Redis;
 use Apache2::Const -compile => qw(FORBIDDEN OK :log);
 
@@ -17,9 +18,7 @@ my $redis = Redis->new(	host=>'localhost',
 				password => $pw,
 				reconnect=>60,
 				every=>5000 );
-if ($redis) {
-	$redis->select(10);
-}
+$redis->select(10) if $redis;
 
 # statsd socket
 my $sock = IO::Socket::INET->new(	PeerPort => 8125,
@@ -32,6 +31,7 @@ sub handler {
 	my $str = $r->connection->client_ip();
 	my $rlog = $r->log;
 
+=comment
 	# let these IPs through anytime
 	if ( 
 		$str =~ /192.168.0/  ||
@@ -46,6 +46,7 @@ sub handler {
 		) {
 		return Apache2::Const::OK;
 	}
+=cut
 
 	# in case redis was not up when apache started, try to connect
 	if (! defined $redis) {		# try to connect
@@ -55,6 +56,7 @@ sub handler {
 					reconnect=>60,
 					every=>5000 );
 		if ($redis) {
+			$redis->auth($pw);
 			$redis->select(10);
 		}
 	}
@@ -71,9 +73,13 @@ sub handler {
 =cut
 
 	my $hostname = $r->hostname();
-
+	my $src = $r->headers_in->get('X-Forwarded-For');
+	#$rlog->notice("--X-Forwarded-For ", $src);
 	#$rlog->notice("---hostname is ", $r->hostname() );
+	my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime();
+	# current minute is in $min
 
+=comment
 	#let these hosts go through
 	if ($hostname =~ /blog.aududu.com/ 
 		|| $hostname =~ /scrappintwins.com/ 
@@ -85,6 +91,7 @@ sub handler {
 		$sock->send( "request.hostname." . $name . ":1|c\n" ) if defined $sock;
 		return Apache2::Const::OK;
 	}
+=cut
 
 	# if there is an attempt to access "zencart/admin" or other attempts, 
 	# then put the ip on the block list
@@ -93,19 +100,27 @@ sub handler {
 		$r->unparsed_uri() =~ /phpbb2/i  ||
 		$r->unparsed_uri() =~ /wp-login/  ||
 		$r->unparsed_uri() =~ /xmlrpc.php/  ||
+		$r->unparsed_uri() =~ /INFORMATION_SCHEMA/  || 
+		$r->unparsed_uri() =~ /CONCAT/  ||
 		$r->unparsed_uri() =~ /phpMyAdmin/ ) {
 		#$sock->send( "hacker.unparsed_uri." . $r->unparsed_uri() . ":1|c\n" ) if defined $sock;
 		if ($redis) {
-			my $key = 'badip.' . $str;
-			#$rlog->notice("putting $key in redis");
-			$redis->auth($pw);
-			$redis->incr( $key );
-			$redis->expire( $key, 86400 );	# 1 day
-			#my $val = localtime(time); # need scalar version of localtime
-			#$redis->set( $key, $val, 'EX', 24*3600 );
-			$sock->send( "request.blocked:1|c\n" ) if defined $sock;
+			my $key = 'badip.' . $src . ":$min";
+			my $result = $redis->get($key);
+			if (! defined($result) || $result < 6) {
+				$redis->multi();
+				$redis->incr($key);
+				$redis->expire( $key, 60 );
+				$redis->exec();
+				$rlog->notice("Allowing $src to " . $r->unparsed_uri());
+				$sock->send( "request.allowed:1|c\n" ) if defined $sock;
+				return Apache2::Const::OK;
+			} else {
+				$rlog->notice("Forbid $src to " . $r->unparsed_uri() . " val " . $result );
+				$sock->send( "request.blocked:1|c\n" ) if defined $sock;
+				return Apache2::Const::FORBIDDEN;
+			}
 		}
-		return Apache2::Const::FORBIDDEN;
 	}
 	
 	$sock->send( "request.allowed:1|c\n" ) if defined $sock;
